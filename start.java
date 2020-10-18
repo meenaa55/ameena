d3dff21ca59987c77c4ed815d7b55f2ddf787879
Ameena <ar314697@gmail.com>
2020-10-18 15:27:13 -0700

    Test code for learning java
    created limelight object and printed degree

---
+++ start.java
@@ -0,0 +1,9 @@
+public class start{
+    public static void main(String[]args){
+        Limelight limelight = new Limelight():
+        something.gettx();
+        print(limelight.gettx());
+
+        System.out.println("Hello World");
+    }
+}
